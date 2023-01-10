946a3f49f3f0719868af02a743d31b400a5b459e
Gabriela Garcia Serrano <118684159+GabrielaSpace@users.noreply.github.com>
2023-01-10 12:31:14 +0100

    sign up form

--- scripts/app.js
+++ scripts/app.js
@@ -1,51 +1,57 @@
-//Phase1
- window.addEventListener('load',()=>{
-    const containerLoader =document.querySelector('#loadercontainer');
-    containerLoader.style.opacity=0;
-    containerLoader.style.visibility = 'hidden'}) 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-analytics.js";
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-let containerPpal=document.querySelector('#dashboard');
-let containerBooks=document.querySelector('#booksSection');
-async function listName() {
-    const result =await fetch (`https://api.nytimes.com/svc/books/v3/lists/names.json?api-key=J3nmH8Nj3Y5btF8WIQMVZohXdMNHAEzW`);
-    const database = await result.json();
-    const listNames = database.results;
+import { getAuth} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js"
+import { createUserWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js"
 
-    for (let i=0; i<listNames.length;i++){containers (listNames[i])}
-    function containers(object,i){
-        let containerU = document.createElement('article');
-        containerU.setAttribute('class','theList');
-        containerPpal.appendChild(containerU);
-        containerU.innerHTML=`<h3>${object.list_name}</h3>
-                            <p>Oldest published: ${object.oldest_published_date}</p>
-                            <p>Latest published: ${object.newest_published_date}</p>
-                            <p>Update frequency: ${object.updated.toLowerCase()}</p>
-                            <button type='button' id='${object.list_name_encoded}'>See more...</button>`
-        document.querySelector(`#${object.list_name_encoded}`).addEventListener('click',()=>{booksLists(object.list_name_encoded,object.list_name);
-        containerPpal.style.display= 'none';
-        containerBooks.style.display='flex';
-    })}}listName() 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBX0Y3sFj84J85Nnu2dqP--4vidaZX0T88",
+  authDomain: "p-nytlibrary-auth.firebaseapp.com",
+  projectId: "p-nytlibrary-auth",
+  storageBucket: "p-nytlibrary-auth.appspot.com",
+  messagingSenderId: "618810417283",
+  appId: "1:618810417283:web:55939d4f0510dd2a40bbb6",
+  measurementId: "G-NVB71RFMP2"
+};
 
-async function booksLists(codeList,name){
-    let resp = await fetch (`https://api.nytimes.com/svc/books/v3/lists/current/${codeList}.json?api-key=J3nmH8Nj3Y5btF8WIQMVZohXdMNHAEzW`)
-    let database = await resp.json();
-    let booksList=database.results.books;
-    containerBooks.innerHTML=`<div id='goBackTitle'><h1>List: ${name}<h1></br></br></br>
-    <button id='goBack' type='button'>Go back</button></div>`
-    document.querySelector('#goBack').addEventListener('click',
-    ()=>{containerBooks.style.display='none';
-        containerPpal.style.display='flex';
-        listName();
-    })
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
+export const auth = getAuth(app)
 
-    for(let i=0; i<booksList.length;i++){bookContainer(booksList[i])}
-    function bookContainer(object){
-        let containerB= document.createElement('article');
-        containerB.setAttribute('class','cover');
-        containerBooks.appendChild(containerB);
-        containerB.innerHTML=`<img src=${object.book_image} alt=${object.title}>
-                            <h3>N°${object.rank}. ${object.title}</h3>
-                            <p>Weeks on list: ${object.weeks_on_list}</p>
-                            <p>${object.description}</p>
-                            <a href=${object.amazon_product_url} target='_blank'>Buy on amazon</a>`}
-}booksLists()  
+
+
+
+
+
+
+
+
+
+const signUpForm =document.querySelector('#signUpForm');
+
+
+signUpForm.addEventListener('submit',async (e)=>{
+    e.preventDefault();
+const signUpUserName =document.querySelector('#userSignUp').value;
+const signUpEmail =document.querySelector('#emailSignUp').value;
+const signUpPassword=document.querySelector('#passwordSignUp').value;
+console.log(signUpEmail,signUpPassword,signUpUserName);
+try {
+    const userCredentials =await createUserWithEmailAndPassword(auth,signUpEmail,signUpPassword)
+console.log(userCredentials)
+} catch (error) {
+    console.log(error.message)
+    console.log(error.code)
+
+    if(error.code==='auth/invalid-email'){
+        alert('invalid email')
+    }if(error.code==='auth/weak-password'){
+        alert('Weak password, the password should be at least 6 characters')
+    }
+}
+})
